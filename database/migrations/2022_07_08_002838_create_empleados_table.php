d70ec8c4ee7ac74a7c4d1858311ae0f60826dd88
José Iván García López <ivangkf323@gmail.com>
2022-07-07 20:07:22 -0500

    fix clean code

--- database/migrations/2022_07_08_002838_create_empleados_table.php
+++ database/migrations/2022_07_08_002838_create_empleados_table.php
@@ -6,11 +6,7 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateEmpleadosTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+
     public function up()
     {
         Schema::create('empleados', function (Blueprint $table) {
@@ -22,11 +18,7 @@ class CreateEmpleadosTable extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
+
     public function down()
     {
         Schema::dropIfExists('empleados');
